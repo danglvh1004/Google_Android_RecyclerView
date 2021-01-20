061f5df23a250a4f736bae84b96fbd964c41b08c
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-20 16:43:45 +0700

    Lab thầy Lâm

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,64 +1,57 @@
 package com.example.recyclerview;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
-
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         for (int i = 0; i < 20; i++) {
-            mWordList.add("Word " + i);
+            mWordList.addLast("Word " + i);
         }
+        mRecyclerView = findViewById(R.id.recylerview); //Tham chiếu đến biến trong file xml
+        mAdapter = new WordListAdapter(this, mWordList); //Truyền context và truyền nội dung
+        mRecyclerView.setAdapter(mAdapter); //Gán recycler view sử dụng adapter
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        mWordList.clear();
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+        mRecyclerView.setAdapter(mAdapter);
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onFABClick(View view) {
+        int wordListSize = mWordList.size();
+        mWordList.addLast("+ Word " + wordListSize);
+        mAdapter.notifyItemInserted(wordListSize);
+        mRecyclerView.smoothScrollToPosition(wordListSize); //cuon cho muot
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        menu.add("Reset").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -1,39 +1,65 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+import com.example.recyclerview.R;
 
-    @NonNull
-    @Override
-    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
+import java.util.LinkedList;
 
-    @Override
-    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
-    }
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    class WordViewHolder extends RecyclerView.ViewHolder {
-
+    class WordViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
+
 
         public WordViewHolder(View itemView, WordListAdapter adapter) {
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
+            itemView.setOnClickListener(this); //bấm vào gọi hàm này
         }
+
+        @Override
+        public void onClick(View view) {
+            int mPosition = getLayoutPosition(); //lấy ra vị trí bấm
+            String element = mWordList.get(mPosition); //Lấy ra dữ liệu bởi hàm get()
+            mWordList.set(mPosition, "Clicked! " + element); //Thay đổi nội dung
+            mAdapter.notifyDataSetChanged(); //Phải gọi mỗi lần thay đổi nội dung
+        }
+    }
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(
+                R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(WordListAdapter.WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
     }
 }
